447291bd18648588521affd7a0bfc48bedd84450
ryoumeki <u.ryo19891108@gmail.com>
2020-04-27 23:52:58 +0900

    gitignore modified

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_22_080655) do
+ActiveRecord::Schema.define(version: 2020_04_27_070508) do
 
   create_table "diaries", force: :cascade do |t|
     t.string "title"
